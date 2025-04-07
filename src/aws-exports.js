e8dd2ffb4919d00871fb68f6d04574caaed28327
theanilsomani <me.anilsomani@gmail.com>
2025-04-07 14:48:52 +0530

    added env variables

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,13 +1,12 @@
 // src/aws-exports.js
 // IMPORTANT: Replace placeholders with your actual Cognito values
-import { Amplify } from "aws-amplify"
 
 const amplifyConfig = {
     Auth: {
         Cognito: {
-            userPoolId: 'ap-south-1_3OVcWWF6e', // e.g., ap-south-1_AbCdEfGhI
-            userPoolClientId: '2ha3busqvod07k3m6cfcki4r9q', // e.g., 1a2b3c4d5e6f7g8h9i0j
-            region: 'ap-south-1' // e.g., ap-south-1 (Optional if derivable)
+            userPoolId: import.meta.env.VITE_AWS_USER_POOL_ID, // Access the env variable
+            userPoolClientId: import.meta.env.VITE_AWS_USER_POOL_CLIENT_ID, // Access the env variable
+            region: import.meta.env.VITE_AWS_REGION // Access the env variable
         }
     }
 };
